0e9c0f099233666445914dee248cd4efb75776db
Emchenko <Emchenko@MAIN>
2020-07-19 12:45:33 +0300

    BME280 working protoype

--- Weather_station2_bme280/Weather_station2_bme280.ino
+++ Weather_station2_bme280/Weather_station2_bme280.ino
@@ -17,46 +17,26 @@
  ***************************************************************************/
 
 #include <Wire.h>
-//#include <SPI.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
+#include <BME280_I2C.h>
 
 #define BME280_ADDRESS (0x76)
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+// Create BME280 object
+BME280_I2C bme(0x76); // I2C using address 0x77
 
 unsigned long delayTime;
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     while(!Serial);    // time to get serial running
     Serial.println(F("BME280 test"));
 
-    unsigned status;
-
-    Wire.begin(D3,D4); 
-    
-    // default settings
-    status = bme.begin(0x76, &Wire);  
-    // You can also pass in a Wire library object like &Wire2
-    // status = bme.begin(0x76, &Wire2)
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-        while (1) delay(10);
-    }
+    if (!bme.begin(D3, D4)) 
+    {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      while (1);
+    } 
     
     Serial.println("-- Default Test --");
     delayTime = 1000;
@@ -66,55 +46,21 @@ void setup() {
 
 
 void loop() { 
-    printValues();
-    readWOlib();
+    //printValues();
+    //readWOlib();
+    printValuesNewLib();
     delay(delayTime);
 }
 
 
-void printValues() {
-    Serial.print("Temperature = ");
-    Serial.print(bme.readTemperature());
-    Serial.println(" *C");
-
-    Serial.print("Pressure = ");
-
-    Serial.print(bme.readPressure() / 100.0F);
-    Serial.println(" hPa");
-
-    Serial.print("Approx. Altitude = ");
-    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-    Serial.println(" m");
-
-    Serial.print("Humidity = ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
-
-    Serial.println();
-}
-
-
-int c = 1;
-int written_bytes = 0;
-byte transmission_status=255;
-
-void readWOlib()
+void printValuesNewLib()
 {
-  //////////////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////////////Read BME Registers/////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////////////
-
-    
-    //Read address of Chip
-    Wire.beginTransmission(0x76);
-    written_bytes=Wire.write(0xD0);
-    transmission_status=Wire.endTransmission();
-    
-    Wire.requestFrom(0x76,1);
-    uint8_t value = Wire.read();
-    
-    Serial.println(value, HEX);
-    Serial.println(written_bytes);
-    Serial.println(transmission_status);
-    
-} 
+  bme.readSensor();
+  Serial.print(bme.getPressure_MB()); Serial.print(" mb\t"); // Pressure in millibars
+  Serial.print(bme.getPressure_P()); Serial.print(" p\t"); // Pressure in pascals
+  Serial.print(bme.getPressure_atm()); Serial.print(" atm\t"); // Pressure in atm
+  Serial.print(bme.getPressure_mmHg()); Serial.print(" mmHg\t"); // Pressure in mmHg
+  Serial.print(bme.getHumidity()); Serial.print(" %\t\t");
+  Serial.print(bme.getTemperature_C()); Serial.print(" *C\t");
+  Serial.print(bme.getTemperature_F()); Serial.println(" *F"); 
+}
