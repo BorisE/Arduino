a33fc5e6e992e1b97b58bbbbf86743dc3d1f669d
Emchenko <Emchenko@MAIN>
2020-08-08 22:58:04 +0300

    watersensor test  -digital read

--- watersensor_test/watersensor_test.ino
+++ watersensor_test/watersensor_test.ino
@@ -1,22 +1,24 @@
 // пин бесконтакного датчика уровня жидкости
 #define LIQIID_SENSOR_APIN A0
-#define LIQIID_SENSOR_PIN D2
+#define LIQIID_SENSOR_PIN D5
  
 void setup()
 {
   // открываем монитор Serial-порта
   Serial.begin(115200);
   // настраиваем пин в режим входа
-  //pinMode(LIQIID_SENSOR_PIN, INPUT);
+  pinMode(LIQIID_SENSOR_PIN, INPUT);
 }
  
 void loop()
 {
   // считываем состояние пина
   int motionState = analogRead(LIQIID_SENSOR_APIN);
-  //int motionState2 = digitalRead(LIQIID_SENSOR_PIN);
+  int motionState2 = digitalRead(LIQIID_SENSOR_PIN);
   // выводим данные в Serial-порт
+  Serial.print("Analog: ");
   Serial.println(motionState);
-  //Serial.println(motionState2);
+  Serial.print("Digital: ");
+  Serial.println(motionState2);
   delay(500);
 }
